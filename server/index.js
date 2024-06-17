d577cbc748071c9e432f9f330f3a2e7dcfc153e3
Serhii Bondarchuk <63853236+Serhii-Bondarchuk@users.noreply.github.com>
2024-06-17 22:36:47 +0300

    fixed deployment

--- server/index.js
+++ server/index.js
@@ -1,42 +1,50 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv'
-import userRoute from './routes/userRoute.js';
-import cors from 'cors'
+// import mongoose from 'mongoose';
+// import bodyParser from 'body-parser';
+// import dotenv from 'dotenv'
+// import userRoute from './routes/userRoute.js';
+// import cors from 'cors'
 import path from 'node:path'
 import { fileURLToPath } from 'url';
 
 
 const app = express();
-app.use(bodyParser.json());
-app.use(cors())
-dotenv.config()
+// app.use(bodyParser.json());
+// app.use(cors())
+// dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const MONGO_URL_RES = "mongodb://localhost:27017/mern-stack"
-const PORT = process.env.PORT || 8000;
-const MONGO_URL = process.env.MONGO_URL || MONGO_URL_RES
-mongoose
-  .connect(MONGO_URL)
-  .then(() => {
-    console.log('MongoDB is connected...')
-    app.listen(PORT, () => {
-      console.log(`Server is starting on ${PORT}`)
-    })
-  })
-  .catch((err) => console.log(err))
+// const MONGO_URL_RES = "mongodb://localhost:27017/mern-stack"
+// const PORT = process.env.PORT || 8000;
+// const MONGO_URL = process.env.MONGO_URL || MONGO_URL_RES
+// mongoose
+//   .connect(MONGO_URL)
+//   .then(() => {
+//     console.log('MongoDB is connected...')
+//     app.listen(PORT, () => {
+//       console.log(`Server is starting on ${PORT}`)
+//     })
+//   })
+//   .catch((err) => console.log(err))
 
 // Have Node serve the files for our built React app
 // app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-app.use("/api", userRoute)
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
+// app.use("/api", userRoute)
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// });
 
 // app.get('/test', (req, res) => res.send('Test Request'))
+
+app.use("/", (req, res) => {
+  res.send('SERVER is running...')
+})
+
+app.listen(5000, (req, res) => {
+  console.log('Server is started on Port 5000...')
+})
